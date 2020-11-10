d2fc5643d62dd8129b05b4712bbe0c05279c841a
Zeal <Jack@x86.sh>
2020-11-10 14:43:15 +1100

    added script

---
+++ parse-subdomains.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import re
+
+
+
+with open('subdomains.txt') as f: 
+	lines = f.read().splitlines()
+
+cleanlist = []
+
+for line in lines: 
+	templist = line.split('<BR>')
+	print templist
+	for item in templist:
+		cleanlist.append(item)
+
+
+r = re.compile("([a-z0-9]+[.])*domain.com")
+
+newlist = list(filter(r.match, cleanlist))
+final_list = sorted(set(newlist))
+
+with open('output.txt', 'w') as o:
+	for i in final_list:
+		o.write("%s\n" % i)
+
